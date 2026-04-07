4735caad84340aa8f1d8b847f3eb73715a1c7943
Ayush soni <ayushsoni.6997@gmail.com>
2026-04-07 09:35:48 +0530

    feat: implement automated CD pipeline and update Terraform infrastructure with Elastic IP support

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-#keys
+# 1. SSH Key Pair
 resource "aws_key_pair" "rapidbase_ssh_key" {
   key_name   = "rapidbase_ssh_key"
   public_key = file("../rapidbase_ssh_key.pub")
@@ -6,35 +6,38 @@ resource "aws_key_pair" "rapidbase_ssh_key" {
     Name = "rapidbase_ssh_key"
   }
 }
-#vpc && security
-resource "aws_default_vpc" "default" {
 
-}
+# 2. Networking (Default VPC)
+resource "aws_default_vpc" "default" {}
 
+# 3. Security Group
 resource "aws_security_group" "rapidbase_sg" {
   name        = "rapidbase_sg"
-  description = "this is a security group using terraform"
+  description = "Security group using terraform for rapidbase"
   vpc_id      = aws_default_vpc.default.id
+
   ingress {
     protocol    = "tcp"
     from_port   = 22
     to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
-    description = "open for ssh"
+    description = "SSH access"
   }
+
   ingress {
     protocol    = "tcp"
     from_port   = 80
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
-    description = "open for http"
+    description = "HTTP access"
   }
+
   ingress {
     protocol    = "tcp"
     from_port   = 5050
     to_port     = 5050
     cidr_blocks = ["0.0.0.0/0"]
-    description = "open for postgresAdmin"
+    description = "postgresAdmin"
   }
 
   ingress {
@@ -42,37 +45,54 @@ resource "aws_security_group" "rapidbase_sg" {
     from_port   = 8001
     to_port     = 8001
     cidr_blocks = ["0.0.0.0/0"]
-    description = "open for redisAdmin"
+    description = "redisAdmin"
   }
+
   egress {
     protocol    = "-1"
     from_port   = 0
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
-    description = "open all inbounds"
+    description = "Allow all outbound traffic"
   }
+
   tags = {
     Name = "rapidbase_sg"
   }
 }
 
-# instance
+# 4. EC2 Instance
 resource "aws_instance" "rapidbase_ec2" {
-  key_name        = aws_key_pair.rapidbase_ssh_key.key_name
-  security_groups = [aws_security_group.rapidbase_sg.name]
-  ami             = var.ami_id
-  instance_type = var.instance_type
-  user_data = file("../scripts/install_docker.sh")
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.rapidbase_ssh_key.key_name
+  vpc_security_group_ids = [aws_security_group.rapidbase_sg.id]
+  user_data              = file("../scripts/install_docker.sh")
 
   root_block_device {
     volume_size = var.root_storage
     volume_type = "gp3"
   }
+
   tags = {
     Name = "rapidbase_ec2"
   }
 }
+
+# 5. Elastic IP
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.rapidbase_ec2.id
+  domain   = "vpc"
+
+  tags = {
+    Name = "rapidbase-EIP"
+  }
+}
+
+# 6. Instance State (Optional, but kept as per your request)
 resource "aws_ec2_instance_state" "all_servers_state" {
   instance_id = aws_instance.rapidbase_ec2.id
   state       = "running"
-}
+}    
+
+
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,11 @@
-output "public_ip" {
-  value = aws_instance.rapidbase_ec2.public_ip
+# 1. The Static IP (Use this for your GitHub Secret: EC2_HOST)
+output "rapidbase_public_ip" {
+  value       = aws_eip.my_static_ip.public_ip
+  description = "The static public IP of the Rapidbase EC2"
 }
-output "public_dns" {
-  value = aws_instance.rapidbase_ec2.public_dns
+
+# 2. The Static Public DNS
+output "rapidbase_public_dns" {
+  value       = aws_eip.my_static_ip.public_dns
+  description = "The public DNS associated with the Elastic IP"
 }
