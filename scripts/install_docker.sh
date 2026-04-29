f4993f10586459dfaef0d6a79997a3aa97f07c36
Ayush soni <ayushsoni.6997@gmail.com>
2026-04-29 12:10:52 +0530

    feat: add automated installation and version verification for kind and kubectl to install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -30,7 +30,21 @@ sudo systemctl enable docker
 # Add ubuntu user to the docker group
 sudo usermod -aG docker ubuntu
 
-# Verify Docker installation
+# installing kind
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# installing kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl 
+
 echo "Installation complete!"
 docker --version
+kind --version
+kubectl version --client
+
+
 newgrp docker
